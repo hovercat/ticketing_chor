089ce6c919dbe329fb5b957d1efbb28651a05bc1
ulrich.aschl@gmail.com <you@example.com>
2022-05-01 14:05:24 +0200

    db layer automap

--- db_map_ticketing/mapper.py
+++ db_map_ticketing/mapper.py
@@ -1,14 +1,26 @@
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
 import sqlalchemy as db
 
-engine = None
-connection = None
-metadata = None
 
-def create_engine(dbconnector):
-    global engine
-    engine = db.create_engine(dbconnector)
-    global connection
-    connection = engine.connect()
-    global metadata
-    metadata = db.MetaData()
+class Mapper:
+    engine = None
+    connection = None
+    metadata = None
+    session = None
+    Base = None
+    Concert = None
+    Reservation = None
 
+    def __init__(self, dbconnector):
+        self.engine = db.create_engine(dbconnector, connect_args={'options': '-csearch_path=ticketing'}) # schema name
+        self.connection = self.engine.connect()
+        self.metadata = db.MetaData()
+
+        self.Base = automap_base()
+        self.Base.prepare(self.engine, reflect=True)
+
+        self.Concert = self.Base.classes.concert
+        self.Reservation = self.Base.classes.reservation
+
+        self.session = Session(self.engine)
