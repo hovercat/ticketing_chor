765c2f0bed996f4395b089106af5a0b8ba81ba73
Kanon Gnaore <simvalue@gmail.com>
2022-05-13 10:30:10 +0200

    added some fancy JS magic

---
+++ frontend/static/js.js
@@ -0,0 +1,44 @@
+var price_student = price_full = seats_left = 0;
+$(document).ready(function() {
+    price_student = parseFloat($('#pl_student').text());
+    price_full = parseFloat($('#pl_full').text()); //storing price in global vars prepares us for dynamic updating later
+    $('.ticket-amount').on('change', updateTotal);
+    $('#sendreservation').prop("disabled", true);
+    updateTotal();
+    updateSeatsLeft();
+});
+
+function updateSeatsLeft() {
+    var selected_concert = parseInt($('#concertdate').val());
+    $.ajax({
+        url: "getseats/"+selected_concert,
+        type: "GET",
+        dataType:"json",
+        success: function(data) {
+            console.log(data);
+            if(!data.failed) {
+                seats_left = data.seats;
+            }
+        }
+    });
+    setTimeout(updateSeatsLeft, 5000);
+}
+
+function updateTotal() {
+    //Also we don't want to do an ajax query everytime number of tickets is changed
+    var n_student = parseInt($('#tickets_student').val());
+    var n_full = parseInt($('#tickets_full').val());
+    n_full = isNaN(n_full) ? 0 : n_full;
+    n_student = isNaN(n_student) ? 0 : n_student;
+    var total_tickets = n_full + n_student;
+    if(total_tickets > 0 || total_tickets > seats_left) {
+        //TODO: Add here some notification if its too many seats
+        $('#sendreservation').prop("disabled", false);
+    } else {
+        $('#sendreservation').prop("disabled", true);
+    }
+
+    var total = n_student*price_student+n_full*price_full;
+    $('#total').text(total);
+};
+
